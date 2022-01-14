37694a10ef04c3d6ba18756beec42504646ec4e5
Rafael <1rafaelsilvalima@gmail.com>
2022-01-14 17:46:26 -0200

    adicionado aulas propriedades constructor e instace off

---
+++ Secao2_objetos/instance_off.js
@@ -0,0 +1,34 @@
+function criarArvore(especie, temFruto) {
+    return {
+        especie: especie,
+        temFruto: temFruto
+    }
+}
+
+
+let laranjeira = criarArvore("Laranjeira", true);
+
+
+function Heroi(nome, classe) {
+    this.nome = nome;
+    this.classe = classe; 
+}
+
+let jaspion = new Heroi("Jaspion", "robô");
+
+
+let = obj = {
+    teste: "teste",
+}
+
+console.log(laranjeira instanceof criarArvore);
+console.log(laranjeira instanceof object);
+console.log(jaspion instanceof Heroi);
+console.log(obj instanceof object);
+
+let arr = [
+
+];
+
+console.log(arr instanceof Array);
+
